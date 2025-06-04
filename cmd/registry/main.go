ba569ce409b5f0a1b40c6cfa9bea36a852717285
Avinash Sridhar <sridharavinash@users.noreply.github.com>
2025-06-04 09:17:51 -0400

    revert to pass linter

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -81,7 +81,7 @@ func main() {
 		}
 	default:
 		log.Printf("Invalid database type: %s; supported types: %s, %s", cfg.DatabaseType, config.DatabaseTypeMemory, config.DatabaseTypeMongoDB)
-		os.Exit(1)
+		return
 	}
 
 	// Initialize authentication services
